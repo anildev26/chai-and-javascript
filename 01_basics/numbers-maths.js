11df6a24bc57f62de6a64f1e9a7dc39e6e69b9da
Anil Sahu <95853576+anildev26@users.noreply.github.com>
2023-09-22 20:11:22 +0000

    file added

---
+++ 01_basics/numbers-maths.js
@@ -0,0 +1,40 @@
+const num = 100
+//console.log(num); //100
+
+const n1 = new Number(1234)
+//console.log(n1); //100
+
+//Number object has some prototype function in it:
+
+const numTry = 100.19313
+
+
+//LocaleString (Country)
+console.log("Locale : "+numTry.toLocaleString("en-IN")) //10,00,000
+
+//convert to string and then we can also use string prototypes in it
+console.log("String : "+numTry.toString()) // 1000000
+
+//toFixed
+console.log("Fixed : "+numTry.toFixed(2)) //100.19313--->100.19 (It doesn't roundOff the decimal digit and just show the number of digits you want after decimal)
+
+//toPrecision Note: Do read mdn javascript documentation for this one
+console.log("Precision : "+numTry.toPrecision(4)) //100.19313--->100.2 (It "roundOff" the decimal digit and just show the number of digits you passed inside function)
+
+
+//***********************maths*********************************
+
+console.log(Math); //Object
+console.log("abs: "+Math.abs(-9)) // 9-->Convert negative to positive
+console.log("Round: "+Math.round(-9.7)) // -9.7 = -10 -->RoundOff the value
+console.log("Ceil: "+Math.ceil(9.1)) // Convert/Roundoff to highest number (ceil word means terrace/highest), even if num extended by .1 it will show the next number 2.1 = 3 (highest num) 
+console.log("Floor: "+Math.floor(9.9)) // Roundoff to Lowest number (floor word means nech/lowest), even if num extended by .1 it will show the lowest number number 2.9 = 2 (lowest num) 
+
+//*********math.random***************
+
+let min = 50;
+let max = 100;
+console.log(Math.floor(Math.random()*(max-min + 1))+ min); // from 50 to 100 random numbers both inclusive which means both 50-100 are included
+
+
+
---
+++ 01_basics/string-mdn/substr-str-slice.js
@@ -0,0 +1,31 @@
+// ******************substring(start, end)************************
+/*
+substring() extracts characters from indexStart up to but not including indexEnd. In particular:
+
+- If indexEnd is omitted, substring() extracts characters to the end of the string.
+- If indexStart is equal to indexEnd, substring() returns an empty string.
+- If indexStart is greater than indexEnd, then the effect of substring() is as if the two arguments were swapped; see example below.
+- Any argument value that is less than 0 or greater than str.length is treated as if it were 0 and str.length, respectively.
+NOTE :- Any argument value that is NaN is treated as if it were 0.
+*/
+
+
+const text = "Chrome"
+
+//The substring() method swaps its two arguments if indexStart is greater than indexEnd, meaning that a string is still returned.
+console.log(text.substring(2, 0))
+
+//negative ex: Negative lengths in substring() will clamp it to 0.
+console.log(text.substring( 4,-4))
+
+
+// ******************substr(start, length)************************
+
+/*ifference between substring() and substr()
+There are subtle differences between the substring() and substr() methods, so you should be careful not to get them confused.
+
+- The two parameters of substr() are ***start and length*** , while for substring(), they are start and end.
+- substr()'s start index will wrap to the end of the string if it is negative, while substring() will clamp it to 0.
+- Negative lengths in substr() are treated as zero, while substring() will swap the two indexes if end is less than start.
+- Furthermore, substr() is considered a legacy feature in ECMAScript, so it is best to avoid using it if possible.
+*/
