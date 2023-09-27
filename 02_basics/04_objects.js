8737f0e0ff437a7034a38e004f898200350baec4
Anil Sahu <95853576+anildev26@users.noreply.github.com>
2023-09-27 02:26:12 +0000

    object part 2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -123,7 +123,25 @@ console.log(users[2].email);// dummy3@oracle.com [Index 2 mtlb array ke andar 3 
 //          Object.values(objname);
 
 // Trying this on first object we created : tinderUser
-console.log(tinderUser); // Original Object
-console.log(Object.keys(tinderUser)); // keys returned as an Array
-console.log(Object.values(tinderUser)); // values returned as an Array
+console.log(tinderUser); //{ id: '123abc', name: 'SammyChat', isLoggedIn: false } --> Original Object
+console.log(Object.keys(tinderUser)); //[ 'id', 'name', 'isLoggedIn' ] --> keys returned as an Array
+console.log(Object.values(tinderUser)); //[ '123abc', 'SammyChat', false ] --> values returned as an Array
 
+// ***************************************************************************************
+// object.entries() : bahot kam use ata hai
+// It converts har ek jo key value pair hai object ke andar usko vo array me bana deta hai
+
+console.log(Object.entries(tinderUser)); //each key value pair into array
+/*
+[
+  [ 'id', '123abc' ],
+  [ 'name', 'SammyChat' ],
+  [ 'isLoggedIn', false ]
+]
+*/
+
+// ***************************************************************************************
+// Check if the object has specified value or not when the object data is really big and not possible to check manually
+// objectName.hasOwnProperty('key Name')  //Determines whether an object has a property with the specified name. Returns Boolean - True/False
+console.log(tinderUser.hasOwnProperty("isLoggedIn")); // true : value exist
+console.log(tinderUser.hasOwnProperty("surname")); // false : valued oesn't exist
