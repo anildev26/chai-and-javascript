eee36970e5b8e3fad9fda9a5a54b076601febacf
Anil Sahu <95853576+anildev26@users.noreply.github.com>
2023-09-28 16:22:58 +0000

    updated

--- 03_basics/02_functions.js
+++ 03_basics/02_functions.js
@@ -14,4 +14,24 @@ function calculateMyCart2(val1, val2, ...prices) {
     return prices
 }
 
-console.log(calculateMyCart2(100, 522, 6956, 78, 4, 96, 455, 200));
+// console.log(calculateMyCart2(100, 522, 6956, 78, 4, 96, 455, 200)); // [ 6956, 78, 4, 96, 455, 200 ] --> As we have only returned the prices from function we are getting data 
+// which is stored in prices parameter, the first 2 passed argument are stored in val1 = 100, val2 = 522 and rest of them are stored in prices. 
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// How can we pass object in functions
+
+const user = {
+    username: "hitesh",
+    price: 799,
+    course: "code aur chai"
+}
+
+function acceptingObject(anyObject){
+    console.log(`My name is ${anyObject.username} and you have applied for "${anyObject.course}"`)
+}
+
+acceptingObject(user);
+
+
+
