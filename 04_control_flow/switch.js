12f5c8d9a9da804167f152dcda30fd0da020fad9
Anil Sahu <95853576+anildev26@users.noreply.github.com>
2023-09-30 22:25:06 +0000

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,30 @@
+// Switch Case
+
+let month = "mar"
+
+switch (month){
+    case 1:
+        console.log("January")
+        break;
+    case "mar":
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("Mar");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+
+    default:
+        console.log("Entered Wrong Month");
+        break;
+}
