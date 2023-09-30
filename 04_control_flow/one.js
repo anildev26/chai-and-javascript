6bc1c7ce23243bdf337fcb3edb357ec7ba08dc4c
Anil Sahu <95853576+anildev26@users.noreply.github.com>
2023-09-30 22:24:23 +0000

    if, if else and conditonal operators

---
+++ 04_control_flow/one.js
@@ -0,0 +1,10 @@
+// If loop
+let a = 16
+let b = 12
+// How to write if loop without parenthesis 
+
+if(a>b) console.log(`executed`);
+
+// how to write if without parenthesis and more than 1 statement (using ",")
+
+if(a>b) console.log(`true`), console.log(`false`); // Both the log will be executed 
