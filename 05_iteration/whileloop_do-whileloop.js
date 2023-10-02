4556e0803a5a52dd674c828a8182c9e7bd6308d3
Anil Sahu <95853576+anildev26@users.noreply.github.com>
2023-10-02 23:02:48 +0000

    while loop and do-while loop

--- 05_iteration/whileloop_do-whileloop.js
+++ 05_iteration/whileloop_do-whileloop.js
@@ -9,7 +9,7 @@ while (num <= 10) {
 
 let myArray = ["superman", "James", "Ben10", "Slugterra"]
 let arr = 0
-while(arr < myArray.length){
+while (arr < myArray.length) {
     console.log(`Value of arr at index - ${arr} : ${myArray[arr]}`);
     arr++
 }
@@ -19,7 +19,7 @@ while(arr < myArray.length){
 // do-while loop
 // Do statement is always executed at least once
 
-do{
- console.log(myArray[arr]);
- arr++
-} while(arr< myArray.length)
+do {
+    console.log(myArray[arr]);
+    arr++
+} while (arr < myArray.length)
