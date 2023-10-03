bbe22ea2889a585a6e9479dbb388a3e779228e67
Anil Sahu <95853576+anildev26@users.noreply.github.com>
2023-10-03 00:55:34 +0000

    filter with implicit - explicit return inside call back function

--- 05_iteration/filter.js
+++ 05_iteration/filter.js
@@ -26,27 +26,27 @@ console.log(arrValFromFilter); // [ 'Steve', 'Donna', 'Louis', 'Pearson' ] ---> 
 
 const myNums = [7, 6, 9, 8, 4, 12, 2, 0, 3, 7, 9, 6, 5, 4, 1, 3, 44]
 const greterNum = myNums.filter((num) => {
-    return num > 10
+  return num > 10
 })
- 
+
 console.log(greterNum); // [ 12, 44 ]
 
 // Filter on book array
 
 const books = [
-    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
-    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
-    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
-    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
-    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
-    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
-    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
-    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
-    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
-  ];
+  { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+  { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+  { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+  { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+  { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+  { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+  { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+  { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+  { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+];
 
 // with return statement
-let userBook = books.filter( (bk) => {return bk.edition > 2010 && bk.genre == 'Science'} ) // {} Scope opened and we can also return in same line
+let userBook = books.filter((bk) => { return bk.edition > 2010 && bk.genre == 'Science' }) // {} Scope opened and we can also return in same line
 // console.log(userBook);
 /*
 [
@@ -66,7 +66,7 @@ let userBook = books.filter( (bk) => {return bk.edition > 2010 && bk.genre == 'S
 */
 
 // without return statement coz we are not defining scope here "{}" and overriting old userBook query :- to overrite it should be declared as let not const
-userBook = books.filter( (bk) => bk.publish<1985)
+userBook = books.filter((bk) => bk.publish < 1985)
 console.log(userBook);
 /* Returned 2 books that have been published before 1985
 [
